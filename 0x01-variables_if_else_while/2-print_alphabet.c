f5e16095141d16b24c77c5add3a75c770cb8a239
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-08-11 16:12:41 -0700

    lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -10,7 +9,7 @@ int main(void)
 char c;
 for (c = 'a' ; c <= 'z' ; c++)
 {
-putchar(c);  
+putchar(c);
 }
 putchar('\n');
 return (0);
