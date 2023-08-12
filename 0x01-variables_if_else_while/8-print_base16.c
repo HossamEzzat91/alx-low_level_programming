e3155a0b147b706a879c4e7efe5256d820ce35b5
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-08-11 17:20:16 -0700

    hexadeximal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ix;
+for (ix = '0' ; ix <= '9' ; ix++)
+{
+putchar(ix);
+}
+for (ix = 'a' ; ix <= 'f' ; ix++)
+{
+putchar(ix);
+}
+putchar('\n');
+return (0);
+}
+
