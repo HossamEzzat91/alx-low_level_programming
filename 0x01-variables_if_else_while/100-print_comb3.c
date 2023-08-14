7f9cba4f0f6ab99032d597795f65012cf273eae5
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-08-13 17:47:50 -0700

    extra

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,13 +8,17 @@ int main(void)
 {
 int ix;
 int n;
-for (n = 0 ; n<= 9 ; n++)
-{   
+for (n = 0 ; n <= 8 ; n++)
+{
 for (ix = 0 ; ix <= 9 ; ix++)
 {
+if (ix <= n)
+{
+ix = n + 1;
+}
 putchar(n + '0');
-putchar(ix + '0'); 
-if (ix != 9)
+putchar(ix + '0');
+if (ix <= 9 && n != 8)
 {
 putchar(',');
 putchar(' ');
