8cee1f570c88ca74902402259cfa918ba1a419c0
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-08-15 21:10:14 -0700

    hours and min

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_sign - check char.
+ *
+ * Return:  1 if positive.
+ *  @n: The number to check
+ */
+void jack_bauer(void)
+{
+int h;
+int m;
+for ( h = 0; h <= 23; h++)
+{
+for (m = 0; m <= 59; m++)
+{
+_putchar(h / 10) + '0')
+_putchar(h % 10) + '0')
+_putchar(':')
+_putchar(m / 10) + '0')
+_putchar(m % 10) + '0')
+_putchar('\n')
+}
+}
+return;
+}
+
+
+
+
+
+
+
+
+
