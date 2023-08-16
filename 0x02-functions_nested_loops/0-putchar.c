6c2e5e3ea0bbbf59187347f0973ad35d234bcb8b
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-08-15 17:47:37 -0700

    commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i;
+char str[]="_putchar";
+while(i <= 7)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+return (0);
+}
