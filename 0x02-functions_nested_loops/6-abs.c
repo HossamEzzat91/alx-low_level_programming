909f7cb6b54eaae988ba4c5126a13f2a5e59888c
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-08-16 17:22:52 -0700

    com

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - check char.
+ *
+ * Return:  absloute value.
+ *  @c: The character to check
+ */
+int _abs(int c)
+{
+return (abs(c));
+
+}
+
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,3 +9,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+int _abs(int);
+#include<stdlib.h>
+#include<stdio.h>
