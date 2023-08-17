d348cd7dfef0373b83250deef89637025609aa06
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-08-16 18:15:01 -0700

    comm

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,41 +1,41 @@
 #include "main.h"
 
 /**
- * times_table - 9 times table.
+ * print_to_98- times table.
  *
  * Return:  void.
- *
+ * @n:tkae the entery num
  */
 void print_to_98(int n)
 {
 int i;
-if ( n <= 98)
+if (n <= 98)
 {
-for ( i = n ; i <= 98 ; i++)
+for (i = n ; i <= 98 ; i++)
 {
-if(i != 98)
+if (i != 98)
 {
-printf("%d",i);
-printf(", ");
-}
-else
-{   
-printf("%d",i);
-}
-}
-}
-else
-{   
-for ( i = n ; i >= 98 ; i--)
-{
-if(i != 98)
-{
-printf("%d",i);
+printf("%d", i);
 printf(", ");
 }
 else
 {
-printf("%d",i);
+printf("%d", i);
+}
+}
+}
+else
+{
+for (i = n ; i >= 98 ; i--)
+{
+if (i != 98)
+{
+printf("%d", i);
+printf(", ");
+}
+else
+{
+printf("%d", i);
 }
 }
 }
