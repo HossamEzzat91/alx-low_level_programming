e4483bd8ff037f9d8420e64f541b87a254a42f44
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-08-17 19:02:54 -0700

    coom

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * print_number -print from 0-9
+ *write characters in lowercase
+ *
+ * Return: void.
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 1; i <= 9; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+return;
+}
