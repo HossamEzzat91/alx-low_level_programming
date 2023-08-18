7ea35c74da1d2f2491e3b7d021c94cb506dbfeec
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-08-17 18:54:45 -0700

    comm

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - check c.
+ *
+ * Return:  1 if lower case.
+ *  @c: The digit to check
+ */
+int _isdigit(int c)
+{
+if (isdigit(c))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,4 @@
 int _putchar(char c);
 int _isupper(int c);
 #include <ctype.h>
+int _isdigit(int c);
