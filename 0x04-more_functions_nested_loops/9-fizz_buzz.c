e3626d3d308af13e41a9e455afee45d631584d7c
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-08-18 06:13:16 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,18 +7,17 @@
 int main(void)
 {
 int i;
-for (i = 1; i < 100;i++)
+for (i = 1; i < 100; i++)
 {
 if (i % 3 != 0 && i % 5 != 0)
 {
-
 printf("%d ", i);
 }
-else if ( i % 3 == 0)
+else if (i % 3 == 0)
 {
 printf("Fizz ");
 }
-else if ( i % 5 == 0)
+else if (i % 5 == 0)
 {
 printf("Buzz ");
 }
