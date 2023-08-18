478c8fdff20891cf679172474454646d71b9c32a
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-08-18 06:18:47 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,17 +13,17 @@ if (i % 3 != 0 && i % 5 != 0)
 {
 printf("%d ", i);
 }
-else if (i % 3 == 0)
-{
-printf("Fizz ");
-}
-else if (i % 5 == 0)
-{
-printf("Buzz ");
-}
 else
 {
-printf("FizzBuzz");
+if (i % 3 == 0)
+{
+printf("Fizz");
+}
+if (i % 5 == 0)
+{
+printf("Buzz");
+}
+printf(" ");
 }
 }
 printf("\n");
