717a79d3c87f00faf8b0e895af203ecffda1d61a
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-08-17 19:12:29 -0700

    coom

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include"main.h"
+/**
+ * print_most_numbers -print from 0-9
+ *write characters in lowercase
+ *
+ * Return: void.
+ */
+void print_most_numbers(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+if (i == 2|| i == 4)
+{
+i++ 
+}
+_putchar(i + '0');
+}
+_putchar('\n');
+return;
+}
+
