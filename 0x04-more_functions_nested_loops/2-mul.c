41d7dbe2277e3237fce7d9a864663739a7c9747e
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-08-17 18:59:18 -0700

    coom

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multi two intger.
+ * @a: take variable
+ * @b: take variable
+ * Return:  result.
+ *
+ */
+int mul(int a, int b)
+{
+int result;
+result = a * b;
+return (result);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,3 +2,4 @@ int _putchar(char c);
 int _isupper(int c);
 #include <ctype.h>
 int _isdigit(int c);
+int mul(int a, int b);
