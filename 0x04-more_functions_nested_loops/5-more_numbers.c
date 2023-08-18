5625890223e2b652bd7d6799461b7276a5308990
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-08-17 19:25:39 -0700

    com

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -17,12 +17,12 @@ for (x = 0; x <= 14; x++)
 
 if (x >= 10)
 {
-_putchar((y / 10) + '0');
-_putchar((y % 10) + '0');
+_putchar((x / 10) + '0');
+_putchar((x % 10) + '0');
 }
 else
 {
-_putchar(y + '0');
+_putchar(x + '0');
 }
 }
 _putchar('\n');
