15d535d8da1ee32acbd8863abadbe357096a1e78
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-08-21 15:02:10 -0700

    coo

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,28 +13,24 @@ if (n > 0 &&  n < 15)
 {
 for (i = 0; i <= n; i++)
 {
-for (x = 0; x <= n; x++)
+for (x = 1; x <= n; x++)
 {
 y = i * x;
 _putchar(',');
 _putchar(' ');
-if (y >= 10 && y < 100 && x != 0)
+if (y >= 10 && y < 100 )
 {
 _putchar(' ');
 _putchar((y / 10) + '0');
 _putchar((y % 10) + '0');
 }
-else if (y >= 100 && x != 0)
+if (y >= 100 && x )
 {
 _putchar((y / 100) + '0');
 _putchar(((y / 10) % 10) + '0');
 _putchar((y % 10) + '0');
 }
-else if (x == 0)
-{
-_putchar(y + '0');
-}
-else if (x != 0)
+else 
 {
 _putchar(' ');
 _putchar(' ');
