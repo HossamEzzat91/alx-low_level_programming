848732f3016e8a0c41c5e4380ae3e7eb259498f0
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-08-21 15:04:35 -0700

    coo

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,11 @@
 #include <unistd.h>
-
 /**
  * reset_to_98 - updates the value it point to to 98
- * @*n: pointer of type int
+ * @n: pointer of type int
  *
  * Return: void
  */
 void reset_to_98(int *n)
 {
-*n=98;
+*n = 98;
 }
