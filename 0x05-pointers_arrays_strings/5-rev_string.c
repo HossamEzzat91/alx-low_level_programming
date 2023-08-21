ebe89c908172a6e88eee0e2ff99ff9d09f91f42b
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-08-21 16:46:53 -0700

    creverse str

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * rev_string - print reverse string follow by newline
+ * @s: pointer of type char
+ * Return: void
+ */
+void rev_string(char *s)
+{
+int len, i, j;
+char c;
+len = strlen(s);
+for (i = 0, j = len - 1; i < j; i++, j--)
+{
+c = s[i];
+s[i] = s[j];
+s[j] = c;
+    }
+}
