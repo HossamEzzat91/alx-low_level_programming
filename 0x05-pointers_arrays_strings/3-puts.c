71f3ce624a1360922d6ff167f9b661a380c8dd85
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-08-21 15:38:16 -0700

    pritn string uisng put function

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - print string follow by newline
+ * @str: pointer of type char
+ * Return: string lenght
+ */
+void _puts(char *str)
+{
+
+puts(str);
+}
+
+
+
+
+
+
+
