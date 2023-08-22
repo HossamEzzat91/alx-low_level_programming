8dd262b479d50d5415df0adc8ded53329d7b39e3
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-08-21 19:47:19 -0700

    com

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts_half - function that prints every other character of a string
+ * @str: pointer of type char
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+int i = 0
+
+for (i = 0 ; i<= n; i++)
+{
+printf('%d', a[i]);
+if (i != n)
+{
+printf(", ");  
+}
+}
+_putchar('\n');
+}
