d0613b44e1f334b181c4b1dfd14d53383b9baa29
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-08-21 19:55:47 -0700

    com

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- * puts_half - function that prints every other character of a string
- * @str: pointer of type char
+ * print_array - function that prints every other character of a string
+ * @a: pointer of type char
+ * @n:array dim
  * Return: void
  */
 void print_array(int *a, int n)
@@ -11,7 +12,7 @@ int i = 0;
 for (i = 0 ;i < n; i++)
 {
 printf("%d", a[i]);
-if (i < n - 1)
+if (i != n - 1)
 {
 printf(", ");  
 }
