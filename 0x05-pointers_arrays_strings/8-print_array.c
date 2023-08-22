cdf0425161877693e587ba516a96ef61c5f471c5
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-08-21 20:05:04 -0700

    com

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,13 +11,13 @@ int i = 0;
 
 for (i = 0; i < n; i++)
 {
-if (i < n - 1)
+if (i != (n - 1))
 {
 printf("%d, ", a[i]);
 }
 else
 {
-printf("%d", a[i]); 
+printf("%d", a[i]);
 }
 }
 _putchar('\n');
