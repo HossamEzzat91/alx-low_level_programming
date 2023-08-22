94b660840f6f5044c7dcc263ee819cfb538c66a9
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-08-21 19:28:40 -0700

    com

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,8 +7,13 @@
 void puts_half(char *str)
 {
 int i = 0;
+int j; 
 int lent = strlen(str);
-
+j = lent / 2;
+if (j % lent != 0)
+{   
+j++
+}  
 for (i = lent / 2 ; str [i] != '\0'; i++)
 {
 _putchar(str[i]);
