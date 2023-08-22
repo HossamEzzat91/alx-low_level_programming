1caac9f36d057bbff334ff035662a63b0e9e7b00
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-08-21 20:12:55 -0700

    com

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * puts_half - function that prints every other character of a string
+ * @str: pointer of type char
+ * Return: void
+ */
+char *_strcpy(char *dest, char *src)
+{
+strcpy(dest, src);
+return (dest);
+}
