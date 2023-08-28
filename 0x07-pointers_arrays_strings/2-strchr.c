5c3d1c965b7cf6898551232e5a3c6e939d2e2775
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-08-28 16:16:58 -0700

    simulition og strsearch fi=unction

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,7 +3,6 @@
  * _strchr- function that locates a character in a string.
  * @s: given string
  * @c : char to search for
- * 
  * Return: char
  */
 char *_strchr(char *s, char c)
@@ -11,7 +10,7 @@ char *_strchr(char *s, char c)
 while (*s != '\0')
 {
 if (*s == c)
-{    
+{
 return (s);
 }
 s++;
