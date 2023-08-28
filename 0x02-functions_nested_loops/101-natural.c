e7a42858eeac4bd6b0cf8149f3692e732117b8a5
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-08-27 18:34:35 -0700

    cool

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,15 +1,13 @@
 #include"main.h"
 /**
- * print_alphabet_x10-lowercase character
- *write characters in lowercase 10 times
+ * main - check the code.
  *
- * Return: void.
+ * Return: Always 0.
  */
-int main (void)
+int main(void)
 {
-
 int y = 0;
-int x = 0; 
+int x = 0;
 while (x < 1024)
 {
 if (x % 3 == 0 || x % 5 == 0)
@@ -18,7 +16,6 @@ y = y + x;
 }
 x++;
 }
-printf ("%d\n", y);
-return 0;
+printf("%d\n", y);
+return (0);
 }
-
