e225b69b200e7e263d4bd28eebcffee7e1c76571
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-08-28 16:39:37 -0700

    strspn simulate

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn- a function that gets the length of a prefix substring.
+ * @s: pointer of memroy adress
+ * @accept : start to count
+ * Return: unsigned integer
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int i = 0, j = 0;
+for (i = 0; s[i] < '\0'; i++)
+{
+while (accept[j] != '\0')
+{
+if (s[i] == accept[j])
+{
+break;
+}
+j++
+}
+if (accept[j] == '\0')
+{
+break;
+}
+i++;
+}
+return (i);
+}
