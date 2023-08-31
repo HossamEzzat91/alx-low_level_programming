4613f5bd765f5dfffcc9b8f6105f7ea14992deea
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-08-30 19:18:41 -0700

    power rase

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _pow_recursion - return value of x rasied to power y
+ * @x: agiven number
+ * @y:power value
+ *  Return: x power n
+ */
+int _pow_recursion(int x, int y)
+{
+int i = x;
+if (x < 0)
+{
+return (-1);
+}
+else if (x == 0)
+{
+return (1);
+}
+else
+{
+i *= factorial(x, y - 1);
+}
+return (i);
+}
