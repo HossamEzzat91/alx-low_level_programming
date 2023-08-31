da27debd1b9832562357c7c11d1318ca816355f3
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-08-30 18:27:41 -0700

    cool

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - print a rev string follow by anew line
+ * @s: string arrays
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+if (*s != '\0')
+{
+_puts_recursion(s + 1);
+_putchar(*s);
+}
+else
+{
+_putchar('\n');
+}
+}
