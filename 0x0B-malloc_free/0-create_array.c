523df2c2389433c9738cf50845dd0958f58d16bb
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-09-04 17:58:22 -0700

    commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,19 +5,19 @@
  * @c: The character to print
  * @size: size of the array
  * Return: pointer to the array.
- * 
+ *
  */
 char *create_array(unsigned int size, char c)
 {
 char *r = malloc(size);
-if( size == 0 || r == 0)
+unsigned int i;
+if (size == 0 || r == 0)
 {
 return (0);
 }
-while (size > 0)
+for (i = 0; i < size; i++)
 {
-r[size] = c;
-size--;
+r[i] = c;
 }
 return (r);
 }
