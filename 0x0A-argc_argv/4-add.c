de4d44b8cdae780d6ffe9463c74b9700228acf8b
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-09-06 19:44:56 -0700

    add int

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc:no of args
+ * @argv: value of args
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+int result = 0;
+int i;
+int num = 0;
+if (argc == 1)
+{
+printf("%d\n",0);
+return(0);
+}
+for (i = 1; i< argc; i++)
+{   
+num = atoi(argv[i]);
+if (num == 0 && argv[i][0] != '0')
+{
+printf("Error\n");
+return (1);
+}
+result += num;
+}
+printf("%d\n", result);
+return (0);
+}
