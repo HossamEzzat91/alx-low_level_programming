ca5b08daf591df1632ef506f66db1d90e79939a3
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-09-06 18:57:29 -0700

    commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,13 +2,13 @@
 
 /**
  * main - Entry point
- *
+ * @argc:no of args
+ * @argv: value of args
  * Return: Always 0 (Success)
  */
-
 int main(int argc, char **argv)
 {
 (void)argc;
 printf("%s\n", argv[0]);
 return (0);
-}	 
+}
