ab9dfb0ab4271429977a50761d9892aed049084e
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-09-06 19:17:30 -0700

    multi of 2 arg

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc:no of args
+ * @argv: value of args
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+int result;
+if (argc == 3)
+{
+result = atoi(argv[1]) *atoi(argv[2]);
+ printf("%d\n", result);
+else
+{
+printf("Error\n");
+return(1):
+}
+return (0);
+}
