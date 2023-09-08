186e49ecb6bf13b4c67b365b29e9b079fcdcad01
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-09-07 17:36:05 -0700

    first

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * str_concat - a function  that returns a pointer to a newly allocated space,
+ * @b: 
+ * Return: pointer to the array.
+ *
+ */
+void *malloc_checked(unsigned int b);
+{
+int  *r;
+r = malloc(r);
+if (r == 0)
+{
+exit (98);
+}
+return (r);
+}
