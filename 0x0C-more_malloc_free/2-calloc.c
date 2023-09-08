2c0f5fc71617c36d4913c3d776927001ebab27db
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-09-07 18:51:02 -0700

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _calloc - a function that locate memory for an array,
+ * @nmemb:number of array elemnts
+ * @size: size of each elemnt
+ * Return: pointer to the array.
+ *
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+void *r;
+unsigned int i;
+if (size == 0 || nmemb == 0)
+{
+return (0);
+}
+r = malloc(nmemb * size);
+if (r == 0)
+{
+return (0);
+}
+
+for (i = 0; i < nmemb; i++)
+{
+((char *)r)[i * size] = 0;
+}
+return (r);
+}
