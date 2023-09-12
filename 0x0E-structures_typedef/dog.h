cde50834d6550e18943d6ceddf19df8ffd62ac11
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-09-11 18:15:56 -0700

    strcut

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - adog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ * Descrption:
+ */
 struct dog
 {
 char *name;
 float age;
 char *owner;
 };
+#endif /* DOG_H */
