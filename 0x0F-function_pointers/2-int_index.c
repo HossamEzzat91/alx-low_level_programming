2be9381195c90a79d0479f33676a8cfb9eff0d5a
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-09-13 18:15:14 -0700

    itrerat

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -7,10 +7,9 @@
  * Return: void.
  *
  */
-int int_index(int *array, int size, int (*cmp)(int));
+int int_index(int *array, int size, int (*cmp)(int))
 {
 int index = -1, i;
-
 if (array != 0 && cmp != 0 && size > 0)
 {
 for (i = 0; i < size; i++)
