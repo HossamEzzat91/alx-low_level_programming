5f9d242fca159d8467f04b480e41036c30f61ee7
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-09-13 17:45:24 -0700

    fun pointer

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,4 +4,5 @@ int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 #include <stddef.h>
 int int_index(int *array, int size, int (*cmp)(int));
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif /* FUNCTION_POINTERS_H */
