ad38abf6594e062669f7c667d3f66b0285fc1bbc
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-09-13 19:38:51 -0700

    simlpe cal

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "calc.h"
+/**
+ * op_add - function add two int
+ * @a: first no
+ * @b:second no
+ * 
+ * Return: the sum.
+ *
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub- function sbtract two int
+ * @a: first no
+ * @b:second no
+ *
+ * Return: the result.
+ *
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_mul - function multiplie two int
+ * @a: first no
+ * @b:second no
+ *
+ * Return: the result.
+ *
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_div - function divided two int
+ * @a: first no
+ * @b:second no
+ *
+ * Return: the result.
+ *
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+ * op_mod - function mod two int
+ * @a: first no
+ * @b:second no
+ *
+ * Return: the result.
+ *
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
+
+
