486a2d73851ada903c2985e9301c7fec732109d6
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-09-13 18:14:12 -0700

    itrerat

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+/**
+ * int_index - writes the character c to stdout
+ * @array: pointer to anme
+ * @cmp:function pointer
+ * @size:array size
+ * Return: void.
+ *
+ */
+int int_index(int *array, int size, int (*cmp)(int));
+{
+int index = -1, i;
+
+if (array != 0 && cmp != 0 && size > 0)
+{
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i] != 0)
+{
+index = i;
+}
+}
+}
+return (index);
+}
