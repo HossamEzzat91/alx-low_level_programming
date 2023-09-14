1d3e7d7bc8e74c20a99d95aff7ac8b8ed30b891c
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-09-13 17:13:00 -0700

    co

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,10 @@
 #include "function_pointers.h"
-
 /**
  * print_name - writes the character c to stdout
  * @name: pointer to anme
  * @f:function pointer
  * Return: void.
- * 
+ *
  */
 void print_name(char *name, void (*f)(char *))
 {
