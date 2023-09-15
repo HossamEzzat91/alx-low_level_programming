701225f3fdc2800800c9c2b9b99c3feb5ce97239
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-09-14 17:46:07 -0700

    0x10-variadic_functions

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#indef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+void print_numbers(const char *separator, const unsigned int n, ...);
+int sum_them_all(const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+#include <stdio.h>
+#include <stdarg.h>
+#endif
