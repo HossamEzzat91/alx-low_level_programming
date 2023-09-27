0a0560af7cc246bbdb33c761500b84b81bf1580c
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-09-26 19:34:12 -0700

    ss

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,13 +8,14 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *current, *temp;
+
 	if (head == NULL || *head == NULL)
 		return;
 	current = *head;
 	while (current != NULL)
 
 	{
-		temp= current;
+		temp = current;
 		current = current->next;
 		free(temp);
 	}
