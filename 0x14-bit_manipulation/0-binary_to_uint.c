ae0f3d11d334af8d9fdf373f80f5f214170e8bea
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-09-29 17:06:41 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *binary_to_unit - function convert binary to dec
+ *binary_to_uint - function convert binary to dec
  *@b:the input binary in digit form
  *Return:the decimal equivalent
  *
@@ -8,13 +8,14 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int dec = 0;
+
 	if (!b)
 		return (0);
-	while(*b != '\0')
+	while (*b != '\0')
 	{
 		if (*b != '0' && *b != '1')
 			return (0);
-		dec = dec * 2 +(*b++ - '0');
+		dec = dec * 2 + (*b++ - '0');
 	}
 	return (dec);
 }
