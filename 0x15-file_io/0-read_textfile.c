829678169ae6029e5b7863c2cf8f074f22824108
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-10-06 18:39:04 -0700

    f

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,14 +8,15 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, nr;
-	char *c ;
+	char *c;
+
 	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
 		return (0);
 	c = malloc(sizeof(char) * letters);
-	if (!c )
+	if (!c)
 		return (0);
 	nr = read(fd, c, letters);
 	nr = write(STDOUT_FILENO, c, nr);
