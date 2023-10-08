e1ea0fb7786d75a8ac7590193e627d7b5a0a7539
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-10-08 16:38:41 -0700

    finally

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -30,6 +30,7 @@ void error_handel(int n1, int n2, char *argv[])
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, err, nread, nwrite;
+	int buf_size = (1024);
 	char buffer[1024];
 
 	if (argc != 3)
@@ -40,14 +41,15 @@ int main(int argc, char *argv[])
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error_handel(file_from, file_to, argv);
-	while ((nread = read(file_from, buffer, 1024)) > 0)
+	while (buf_size == 1024)
 	{
+		nread = read(file_from, buffer, 1024);
+		if (nread == -1)
+			error_handel(-1, 0, argv);
 		nwrite = write(file_to, buffer, 1024);
 		if (nwrite == -1)
 			error_handel(0, -1, argv);
 	}
-	if (nread == -1)
-		error_handel(-1, 0, argv);
 	err = close(file_from);
 	if (err == -1)
 	{
