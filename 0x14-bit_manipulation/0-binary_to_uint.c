c57b46114a40361c57677dc0caa78f8ba1d8d230
HossamEzzat91 <hossam.ezzat@hotmail.com>
2023-10-26 18:53:43 -0700

    fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,14 +9,12 @@
 
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int result = 0;
+        int i = 0;
 	if (b == NULL) 
 	{
-	return 0;
+	return (0);
 	}
-	
-    	unsigned int result = 0;
-	int i = 0;
-
 	while (b[i] != '\0')
 	{
 	if (b[i] == '0' || b[i] == '1')
@@ -26,9 +24,9 @@ unsigned int binary_to_uint(const char *b)
 	}
 	else 
 	{
-		return 0;
+		return (0);
 	}
 	}
 
-    return result;
+    return (result);
 }
